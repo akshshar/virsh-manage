9c214ea7ee28c89621a4bbc6d77c5ac7d5a385b9
Akshat Sharma <akshat31187@gmail.com>
2018-08-10 03:11:39 -0700

    Add files via upload

---
+++ hello-ydk.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+#
+# Copyright 2016 Cisco Systems, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# hello-ydk.py
+# Read all data for model Cisco-IOS-XR-shellutil-oper and print system
+# uptime.
+#
+
+from argparse import ArgumentParser
+from urlparse import urlparse
+
+# import providers, services and models
+from ydk.services import CRUDService
+from ydk.providers import NetconfServiceProvider
+from ydk.models.cisco_ios_xr import Cisco_IOS_XR_shellutil_oper \
+    as xr_shellutil_oper
+from datetime import timedelta
+
+
+if __name__ == "__main__":
+    """Main execution path"""
+
+    parser = ArgumentParser()
+    parser.add_argument("-v", "--verbose", help="print debugging messages",
+                        action="store_true")
+    parser.add_argument("device",
+                        help="NETCONF device (ssh://user:password@host:port)")
+    args = parser.parse_args()
+    device = urlparse(args.device)
+
+    # log debug messages if verbose argument specified
+    if args.verbose:
+        logger = logging.getLogger("ydk")
+        logger.setLevel(logging.INFO)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(("%(asctime)s - %(name)s - "
+                                      "%(levelname)s - %(message)s"))
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    # create NETCONF provider
+    provider = NetconfServiceProvider(address=device.hostname,
+                                      port=device.port,
+                                      username=device.username,
+                                      password=device.password,
+                                      protocol=device.scheme)
+
+
+    # create CRUD service
+    crud = CRUDService()
+
+    # create system time object
+    system_time = xr_shellutil_oper.SystemTime()
+
+    # read system time from device
+    system_time = crud.read(provider, system_time)
+
+    # print system uptime
+    print("System uptime is " + 
+          str(timedelta(seconds=int(system_time.uptime.uptime.get()))))
+
+    # End of script
